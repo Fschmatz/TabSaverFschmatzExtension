f2e3e747ee2a67a3668ba2acf7a65e870f3fec37
Fernando <fschmatz@hotmail.com>
2022-01-07 13:11:59 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const saveTabBtn = document.getElementById("tab-btn")
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+//EXIBIR LISTA
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+//SALVAR TAB ATUAL
+saveTabBtn.addEventListener("click", function () {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
+
+    //inputEl.value = ""
+
+})
+
+
+
+/*inputBtn.addEventListener("click", function () {
+    if(inputEl.value){
+        myLeads.push(inputEl.value)
+        //save
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+        inputEl.value = ""
+    }
+})*/
+
+//6h 46m
