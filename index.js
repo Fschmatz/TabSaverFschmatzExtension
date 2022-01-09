29cfaa1e45b3452eea939b660c534abde115d96e
Fernando <fschmatz@hotmail.com>
2022-01-09 17:16:30 -0300

    v 1.6.0 - exibir somente o title da url na lista - alteração no localStorage

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const ulEl = document.getElementById("ul-el")
 let linksFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"))
 const saveTabBtn = document.getElementById("tab-btn")
 
-// console.log( tabs[0].title)
-
-
 if (linksFromLocalStorage) {
     myLinks = linksFromLocalStorage
     render(myLinks)
@@ -19,8 +16,8 @@ function render(links) {
         listItems += `
         <li>
         <div id="listItem">
-            <a target='_blank' href='${links[i]}'>
-                ${links[i]}
+            <a target='_blank' href='${links[i].split(';')[0]}'>
+                ${links[i].split(';')[1]}
             </a>
            <button id="deleteItem-btn${[i]}" class="box" ><i class="fa fa-trash fa-lg"></i></button>        
         </div>
@@ -45,8 +42,7 @@ clearListBtn.addEventListener("dblclick", function () {
 //SALVAR TAB ATUAL
 saveTabBtn.addEventListener("click", function () {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-
-        myLinks.push(tabs[0].url)
+        myLinks.push(tabs[0].url + ";"+tabs[0].title)
         localStorage.setItem("myLinks", JSON.stringify(myLinks))
         render(myLinks)
     })
@@ -54,11 +50,10 @@ saveTabBtn.addEventListener("click", function () {
 
 function deleteItemFromLinks(evt) {
 
-    //TRAMBIQUE
+    //TRAMBIQUE para q ocorra somente com o .box
     let itemToDelete = evt.currentTarget.valueOf().id
     let x = itemToDelete.replace('deleteItem-btn','');
     myLinks.splice(x,1)
-
 
     localStorage.setItem("myLinks", JSON.stringify(myLinks))
     render(myLinks)
